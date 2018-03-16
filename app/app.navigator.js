3d7b5d80d4ecad066b1bed8ecc8fdbd8fe0d42d5
Harish Kumar <echarish@gmail.com>
2018-03-16 10:51:43 +0900

    Created till Match screen

--- app/app.navigator.js
+++ app/app.navigator.js
@@ -1,29 +1,45 @@
 import React from 'react'
-import {DrawerNavigator} from 'react-navigation'
+import {DrawerNavigator, StackNavigator} from 'react-navigation'
+
 import SplashScreen from './screens/splash.screen'
 import LoginScreen from './screens/login.screen'
+import MatchScreen from './screens/match.screen'
 
-const Splash =  {
+const Splash = {
     screen: SplashScreen,
     navigationOptions: {
         header: null
     }
 }
 
-const Login =  {
+const Login = {
     screen: LoginScreen,
     navigationOptions: {
         header: null
     }
 }
 
+const Match = {
+    screen: MatchScreen,
+    navigationOptions: {
+        headerMode: 'screen',
+        headerTitle: 'Matches',
+        drawerLabel: 'Matches'
+    }
+}
+
+const MatchStack = StackNavigator({
+    Match: Match
+}, {})
+
 const RouteConfig = {
     initialRoute: 'Splash'
 }
 
-const AppNavigator =  DrawerNavigator({
+const AppNavigator = DrawerNavigator({
     Splash: Splash,
-    Login: Login
+    Login: Login,
+    Match: {screen: MatchStack}
 }, RouteConfig)
 
 export default AppNavigator;
---
+++ app/components/match.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+export default class Match extends Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return null;
+    }
+}
